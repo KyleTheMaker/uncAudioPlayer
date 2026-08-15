bd7eeb3b88b1edd04f9ca791ac223c8e6c3825fd
KyleTheMaker <kyle.wj.maddox@gmail.com>
2026-08-15 16:00:36 -0700

    playlist is now in alphabetical order

--- src/components/PlayList.tsx
+++ src/components/PlayList.tsx
@@ -37,7 +37,14 @@ const PlayList = () => {
 
   const loadSongs = async () => {
     const allSongs = await getPlayListSongs(db);
-    setSongsList(allSongs);
+    const sortedSongs = allSongs.sort(function(a,b){
+      let x = a.name.toLowerCase();
+      let y = b.name.toLowerCase();
+      if(x < y){return -1;}
+      if(x > y){return 1;}
+      return 0;
+    });
+    setSongsList(sortedSongs);
   };
 
   const handleRemoveSong = async (name: string) => {
