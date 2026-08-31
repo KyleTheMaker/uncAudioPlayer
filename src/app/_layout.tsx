c5fd9cc827db04c46cd7fc8f6acfeb21a6024063
KyleTheMaker <kyle.wj.maddox@gmail.com>
2026-08-31 16:31:34 -0700

    unified tab design with app themes

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -12,13 +12,15 @@
 import { Ionicons } from "@expo/vector-icons";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SongProvider } from "@/context/SongContext";
-import { ThemeProvider } from "@/context/ThemeContext";
+import { ThemeProvider} from "@/context/ThemeContext";
+import  AppTabs  from "@/components/Tabs";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { SQLiteProvider } from "expo-sqlite";
 import { Tabs } from "expo-router";
 
 import { manageDBIfNeeded } from "@/data/musicdb";
 import { MediaControlProvider } from "@/context/MediaControlContext";
+import { View } from "react-native";
 
 export default function App() {
   return (
@@ -28,50 +30,7 @@ export default function App() {
             <MediaControlProvider>
             <SongProvider>
                 <ThemeProvider>
-                <Tabs>
-                <Tabs.Screen
-                    name='index'
-                    options={{
-                        title: "Home",
-                        headerShown: false,
-                        tabBarLabel: "Index",
-                        tabBarIcon: ({ color, size }) => (
-                            <Ionicons name='home' size={size} color={color} />
-                        )}}
-                />
-                <Tabs.Screen
-                    name="PlayListScreen"
-                    options={{
-                        title: "Playlist",
-                        headerShown: false,
-                        tabBarLabel: "Playlist",
-                        tabBarIcon: ({ color, size }) => (
-                            <Ionicons name='albums' size={size} color={color} />
-                        )}}
-                    />
-                <Tabs.Screen
-                    name="MediaPlayerScreen"
-                    options={{
-                        title:"Audio Player", 
-                        headerShown: false,
-                        tabBarLabel: "Player",
-                        tabBarIcon: ({ color, size }) => (
-                            <Ionicons name='musical-notes' size={size} color={color} />
-                        )
-                    }}
-                        />
-                <Tabs.Screen
-                    name="SettingsScreen"
-                    options={{
-                        title:"Settings", 
-                        headerShown: false,
-                        tabBarLabel: "Settings",
-                        tabBarIcon: ({ color, size }) => (
-                            <Ionicons name='settings' size={size} color={color} />
-                        )
-                    }}
-                />
-            </Tabs>
+                    <AppTabs />                    
             </ThemeProvider>
             </SongProvider>
           </MediaControlProvider>
---
+++ src/components/Tabs.tsx
@@ -0,0 +1,67 @@
+import { Ionicons } from "@expo/vector-icons";
+import { useThemeStyles } from "@/context/ThemeContext";
+import { Tabs } from "expo-router";
+import { View } from "react-native";
+
+
+
+
+export default function AppTabs() {
+    const theme = useThemeStyles();
+return (
+    <Tabs 
+        screenOptions={{
+            tabBarStyle: {
+                backgroundColor: theme.colors.background,
+                borderTopColor: "transparent",
+                elevation: 0,
+            },
+            
+            tabBarActiveTintColor: theme.colors.accent,
+            tabBarInactiveTintColor: theme.colors.primary
+        }}
+    >
+        <Tabs.Screen
+            name='index'
+            options={{
+                title: "Home",
+                headerShown: false,
+                tabBarLabel: "Index",
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name='home' size={size} color={color} />
+                )}}
+        />
+        <Tabs.Screen
+            name="PlayListScreen"
+            options={{
+                title: "Playlist",
+                headerShown: false,
+                tabBarLabel: "Playlist",
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name='albums' size={size} color={color} />
+                )}}
+            />
+        <Tabs.Screen
+            name="MediaPlayerScreen"
+            options={{
+                title:"Audio Player", 
+                headerShown: false,
+                tabBarLabel: "Player",
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name='musical-notes' size={size} color={color} />
+                )
+            }}
+                />
+        <Tabs.Screen
+            name="SettingsScreen"
+            options={{
+                title:"Settings", 
+                headerShown: false,
+                tabBarLabel: "Settings",
+                tabBarIcon: ({ color, size }) => (
+                    <Ionicons name='settings' size={size} color={color} />
+                )
+            }}
+        />
+    </Tabs>
+)}
